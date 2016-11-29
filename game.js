5cc04a6c2c3ff66fe60a4de5fa6dd5cd3f117a17
Gloriana <glorygarro@gmail.com>
2016-11-29 17:12:29 +0000

    avance movimientos

---
+++ game.js
@@ -0,0 +1,206 @@
+/**
+Variables globales
+**/
+
+var imgWidth = 80;
+var imgHeight = 80;
+var activePlayers = [];
+var cells = [];
+var availableColors = ["red","yellow","green","blue"];
+var addingPlayers = 'true';
+var upperSpace  = document.getElementById("upperSpace");
+var enter =  document.getElementById("enter");
+var questionsPool = []; 
+
+
+/**
+**/
+
+var enterOffset = getOffset(enter);
+var cellWidth = enterOffset.width; 
+var cellHeight = enterOffset.height; 
+
+/**
+ Propiedades
+
+**/
+
+function property(type, id, propertyBuy, propertySell, houseBuy, houseSell, h0, h1, h2, h3, h4, h5 ){ //h0, h1, h2 ... corresponden a los hospedajes de cada casa 
+	this.type = type; //property, enter, cave, hotChair
+	this.id = id; 
+	if (type == "property"){
+
+		this.propertyBuy = propertyBuy;
+		this.propertySell = propertySell;
+		this.houseBuy = houseBuy; 
+		this.houseSell = houseSell;
+		this.h0 = h0;
+		this.h1 = h1; 
+		this.h2 = h2; 
+		this.h3 = h3; 
+		this.h4 = h4; 
+		this.h5 = h5;
+	} 
+
+}
+
+
+/**
+Objeto player contiene la informacion del jugador, como su numero, nombre, puntaje, dinero liquido
+**/
+/**
+function Player(number, name, color, liquidCash, richness, position ){
+	this.number = number;
+	this.name = name; 
+	this.color = color; 
+	this.liquidCash = liquidCash; 
+	this.richness = richness; 
+	this.position = position; 
+	this.img = document.createElement('img');
+	this.img.src = ('img/'+color+'.png');
+	this.img.setAttribute("height",imgHeight);
+    this.img.setAttribute("width",imgWidth);
+}
+**/
+
+/**
+Funciones auxiliares
+**/
+
+function removeFromArray(srcArray, item){
+
+	var index = srcArray.indexOf(item);
+	
+	if (index!=-1){
+
+		srcArray.splice(index, 1);
+	}
+	
+
+}
+ 
+
+function getOffset(el) {
+  el = el.getBoundingClientRect();
+  
+  return {
+    left: el.left + window.scrollX,
+    top: el.top + window.scrollY,
+	height: el.height,
+	width: el.width
+  }
+}
+/**
+Agregar jugadores 
+**/
+function addPlayer(playerNumber){
+	var alreadyExists = playerExists(playerNumber);
+	console.log(alreadyExists);
+	if(!alreadyExists){
+		if (addingPlayers){
+			if(activePlayers.length < 4){
+				var player = {};
+				player.name = prompt("Por favor ingrese el nombre para el jugador "+playerNumber);
+				player.number = playerNumber;
+				player.liquidCash = 650;
+				player.richness = 650;
+				document.getElementById("name"+playerNumber).innerHTML = player.name;
+				//player.position = "enter";
+				activePlayers.push(player);
+				loadColorSelector(player.name);
+				
+			}
+		}
+	}
+}
+
+function loadColorSelector( name){
+	var divSelectColor = document.createElement('div');
+	var msg = document.createTextNode(name+' , seleccione un color');
+	divSelectColor.appendChild(msg);
+	var upperSpaceOffset = getOffset(upperSpace);
+	divSelectColor.setAttribute("width",upperSpaceOffset.width - 20);
+	divSelectColor.setAttribute("height",upperSpaceOffset.height - 20);
+	divSelectColor.id = "colorSelector";
+	for(var i  = 0; i < availableColors.length; i++){
+		img = document.createElement('img');
+		img.src = ('img/'+availableColors[i]+'.png');
+		img.setAttribute("height",upperSpaceOffset.height-80);
+		img.setAttribute("width",(upperSpaceOffset.width/4)-40);
+		
+		img.id=availableColors[i];
+		//img.onclick = function (availableColors[i] ){ alert("Ha escogido el color " + availableColors[i]); color =availableColors[i]; }
+		img.onclick = selectColor;
+		divSelectColor.appendChild(img);
+	}
+	upperSpace.appendChild(divSelectColor);
+}
+function selectColor ( ){
+
+	if(addingPlayers){
+		color = this.id;
+	if (color != null){
+		activePlayers[activePlayers.length-1].color = color;
+		activePlayers[activePlayers.length-1].img = this;
+		removeFromArray(availableColors, color);
+		this.parentNode.style.display = 'none';
+		var enterOffset = getOffset(enter);
+		this.setAttribute("height",enterOffset.height/5);
+		this.setAttribute("width",enterOffset.width/3);
+		this.style.left = enterOffset.left + enterOffset.width/3;
+		this.style.top = enterOffset.top + ((enterOffset.height/5) * activePlayers.length); 
+		this.onclick = none; //cqmbiar
+		enter.appendChild(this);
+		console.log(activePlayers);
+		/*var colorSelector = document.getElementById("colorSelector");
+		console.log(colorSelector);
+		colorSelector.style.display = 'none';*/
+	}
+	}
+}
+
+function none(){
+//cambiar
+}
+
+function playerExists(playerNumber){
+	
+	for (var i=0; i < activePlayers.length; i++ ){
+		if(activePlayers[i].number == playerNumber){
+			console.log('jale buchon');
+			return true;
+		}
+	}
+
+	return false;
+}
+
+function getPlayer(playerNumber){
+	
+	for (var i = 0; i < activePlayers.length; i++){
+		if (activePlayers[i].number == playerNumber){
+			return activePlayers[i];
+		}
+	}
+}
+/**
+Mover jugadores
+**/
+
+
+function movePlayer(playerNumber, numberOfCells){
+	var thisPlayer = getPlayer(playerNumber);
+
+
+}
+
+
+function calculateSequence (startingCell, numberOfCells)
+{
+
+}
+
+function moveLeft(player, numberOfCells){
+
+
+}
